5dbe4ce8f7978b2e80e90cadb46f88e56cbd009b
Amit Rai <arajanrai@gmail.com>
2024-04-27 13:51:15 +0530

    Working on Wallpaper Categories
    Cards with background images and text in grid is created.
    Precaching needs to be done

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -2,6 +2,7 @@
 
 import 'dart:convert';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -12,14 +13,30 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Map<String, dynamic>? jsonArray;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+class Wallpaper {
+  final String name;
+  final String url;
+
+  const Wallpaper(this.name, this.url);
+
+  factory Wallpaper.fromJson(Map<String, dynamic> json) {
+    final name = json['name'] as String;
+    final url = json['url'] as String;
+    return Wallpaper(name, url);
   }
+}
+
+class WallpapersArray {
+  final List<Wallpaper> wallpapers;
+
+  const WallpapersArray(this.wallpapers);
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final jsonArray = <Map<String, List<Wallpaper>>>[];
+  List<String> keys = [];
+
 
   @override
   void initState() {
@@ -30,11 +47,23 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> _loadJsonFromAssets() async {
     try {
       String jsonString = await rootBundle.loadString('assets/fileList.json');
-      setState(() {
-        jsonArray = jsonDecode(jsonString)['wallpapersArray'];
-      });
-    } catch (e) {
-      print('Error loading JSON: $e');
+      final jsonData = jsonDecode(jsonString);
+      if (jsonData is Map<String, dynamic>) {
+        if (jsonData.containsKey('wallpapersArray')) {
+          final wallpapersArray = jsonData['wallpapersArray'] as Map<String, dynamic>;
+          wallpapersArray.forEach((key, value) {
+            if (value is List<dynamic>) {
+              final processedWallpapers = value.map((wallpaperJson) => Wallpaper.fromJson(wallpaperJson)).toList();
+              jsonArray.add({key: processedWallpapers});
+            }
+          });
+          setState(() {
+            keys = wallpapersArray.keys.toList();
+          });
+        }
+      }
+    } finally {
+      setState(() {}); // Update UI regardless of success or failure
     }
   }
 
@@ -46,23 +75,46 @@ class _MyHomePageState extends State<MyHomePage> {
             .of(context)
             .colorScheme
             .inversePrimary,
-        title: Text(widget.title),
+        title: Text(widget.title, style: TextStyle(color: Theme.of(context).colorScheme.primary),),
       ),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
+          crossAxisSpacing: 0,
+          mainAxisSpacing: 0,
         ),
-        itemCount: jsonArray!.keys.length,
+        itemCount: keys.length,
         itemBuilder: (context, index) {
-          return Card.filled(
-              child: Column(
-                children: <Widget>[
-                  ListTile(
-                    title: Text(jsonArray!.entries.elementAt(index).key),
+          // String imageUrl = jsonArray!.entries.elementAt(index).value;
+          return InkWell(
+                onTap: () {
+
+                },
+                child: SizedBox(
+                  height: 10,
+                  child: Card.filled(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: CachedNetworkImageProvider(
+                            jsonArray.elementAt(index).values.first.elementAt(0).url,
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          keys.elementAt(index).toUpperCase().split("_").join(" "),
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(color: Colors.white70, fontSize: 26, fontWeight: FontWeight.w900),
+                        ),
+                      ),
+                    ),
                   ),
-                ],
               ),
             );
           }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:onemb_wallpapers/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ONEMBWallpapersApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
